711dc83fbf445494a9274b890c7f40ae8e18fd0f
Bogdan Barbu <45259752+bogdanbeniaminb@users.noreply.github.com>
2023-04-24 20:27:49 +0300

    moved events to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 /// <reference types="google.maps" />
 
+import { LocationContainerSettings, Position, SearchResult, LocationData } from './types/interfaces.d';
+import LocationsMap from './LocationsMap';
+import MapsWrapperInterface, { MapMarkerInterface } from './map-providers/MapsWrapperInterface.d';
+
 export { default as default } from './LocationsMap';
 export { default as LocationsMap } from './LocationsMap';
 
@@ -30,8 +34,6 @@ export type {
   SearchProvider,
 } from './types/interfaces';
 
-import {} from './types/events.d';
-
 export { default as Pagination, defaultSettings as PaginationDefaultSettings } from './pagination-provider/Pagination';
 export type { PaginationProvider, PaginationSettings } from './pagination-provider/PaginationProvider.d';
 
@@ -59,3 +61,115 @@ type GoogleInfoWindow = google.maps.InfoWindow;
 export type { MapOptions, GoogleMap, GoogleMarker, GoogleInfoWindow, GoogleIcon };
 
 export { distance } from './utils/locations-utils';
+
+declare global {
+  interface ElementEventMap {
+    'initializing.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      settings: LocationContainerSettings;
+    }>;
+    'initialized.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      settings: LocationContainerSettings;
+    }>;
+    'parseLocations.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      locations: LocationData[];
+    }>;
+    'appliedFilters.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      filters: string[];
+    }>;
+    'updatedLocations.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      locations: LocationData[];
+    }>;
+    'updatedLocationListContent.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+    }>;
+    'updatedLocationsCount.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      count: number;
+      html: string;
+      template: HTMLElement | null;
+    }>;
+    'search.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+    }>;
+    'updatingFromSearch.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      result: SearchResult;
+    }>;
+    'updatedFromSearch.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      result: SearchResult;
+    }>;
+    'geolocated.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      position: GeolocationPosition;
+    }>;
+    'showPopup.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      marker: MapMarkerInterface;
+      location: LocationData;
+      popupHtml: string;
+    }>;
+    'showPopupOnMap.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      marker: MapMarkerInterface;
+      location: LocationData;
+      popupHtml: string;
+    }>;
+    'showPopupOutsideMap.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      marker: MapMarkerInterface;
+      location: LocationData;
+      popupHtml: string;
+    }>;
+    'listClick.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      originalEvent: MouseEvent;
+      location: LocationData;
+      locationId: string;
+      mapWrapper: MapsWrapperInterface;
+    }>;
+    'listHover.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      originalEvent: MouseEvent;
+      location: LocationData;
+      locationId: string;
+      mapWrapper: MapsWrapperInterface;
+    }>;
+    'updatedMapPosition.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      position: GeolocationPosition | Position;
+      firstTime: boolean;
+    }>;
+    'replaceHTMLPlaceholders.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      html: string;
+      location: LocationData;
+    }>;
+    'generateLocationHTML.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      html: string;
+      innerHtml: string;
+      isSelected: boolean;
+      location: LocationData;
+    }>;
+    'generateLocationPopupHTML.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      html: string;
+      innerHtml: string;
+      location: LocationData;
+    }>;
+    'updatingContent.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      keepPopupsOpen: boolean;
+    }>;
+    'updatedContent.locationsMap': CustomEvent<{
+      locationsMap: LocationsMap;
+      keepPopupsOpen: boolean;
+    }>;
+  }
+}
